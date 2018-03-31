b6dd5151882c5c0bc137a74f8b17251ead54f607
mariano <desarrollosur@yahoo.com.ar>
2018-03-30 22:15:07 -0300

    inicialización del proyecto

---
+++ index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Bootstrap 4, from LayoutIt!</title>
+
+        <meta name="description" content="Source code generated using layoutit.com">
+        <meta name="author" content="LayoutIt!">
+
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h3 class="text-center">
+                        Batalla Naval Curza 0.0.1
+                    </h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <ul class="nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Profile</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#">Messages</a>
+                        </li>
+                        <li class="nav-item dropdown ml-md-auto">
+                            <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">Dropdown link</a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
+                                <div class="dropdown-divider">
+                                </div> <a class="dropdown-item" href="#">Separated link</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>
+                                    #
+                                </th>
+                                <th>
+                                    Barco
+                                </th>
+                                <th>
+                                    Coordenadas
+                                </th>
+                                <th>
+                                    Alineación
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="table-active">
+                                <td>
+                                    1
+                                </td>
+                                <td>
+                                    Barco 1
+                                </td>
+                                <td>
+                                    <input size="2" type="text" /> -
+                                    <input size="2" type="text" />
+                                </td>
+                                <td>
+                                    <select>
+
+                                        <option>
+                                            Horizontal
+                                        </option>
+                                    </select>
+                                </td>
+                            </tr>
+                            <tr class="table-active">
+                                <td>
+                                    2
+                                </td>
+                                <td>
+                                    Barco 2
+                                </td>
+                                <td>
+                                    <input size="2" type="text" /> -
+                                    <input size="2" type="text" />
+                                </td>
+                                <td>
+                                    <select>
+
+                                        <option>
+                                            Horizontal
+                                        </option>
+                                    </select>
+                                </td>
+                            </tr>
+                            <tr class="table-active">
+                                <td>
+                                    3
+                                </td>
+                                <td>
+                                    Barco 3
+                                </td>
+                                <td>
+                                    <input size="2" type="text" /> -
+                                    <input size="2" type="text" />
+                                </td>
+                                <td>
+                                    <select>
+
+                                        <option>
+                                            Vertical
+                                        </option>
+                                    </select>
+                                </td>
+                            </tr>
+                            <tr class="table-active">
+                                <td>
+                                    4
+                                </td>
+                                <td>
+                                    Barco 4
+                                </td>
+                                <td>
+                                    <input size="2" type="text" /> -
+                                    <input size="2" type="text" />
+                                </td>
+                                <td>
+                                    <select>
+
+                                        <option>
+                                            Horizontal
+                                        </option>
+                                    </select>
+                                </td>
+                            </tr>
+                            <tr class="table-active">
+                                <td>
+                                    5
+                                </td>
+                                <td>
+                                    Barco 5
+                                </td>
+                                <td>
+                                    <input size="2" type="text" /> -
+                                    <input size="2" type="text" />
+                                </td>
+                                <td>
+                                    <select>
+
+                                        <option>
+                                            Horizontal
+                                        </option>
+                                    </select>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-md-6">
+                    <img alt="Bootstrap Image Preview" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Schiffeversenk.jpg/220px-Schiffeversenk.jpg" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <form role="form">
+                        <div class="form-group">
+
+                            <label for="exampleInputEmail1">
+                                Usuario
+                            </label>
+                            <input class="form-control" id="exampleInputEmail1" type="email" />
+                        </div> 
+                        <button type="submit" class="btn btn-primary">
+                            Submit
+                        </button>
+                    </form>
+                </div>
+                <div class="col-md-6">
+                </div>
+            </div>
+        </div>
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>

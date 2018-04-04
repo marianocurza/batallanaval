b4ca126ef06861eb0d78f21146e4f35bb754a2b1
mariano <desarrollosur@yahoo.com.ar>
2018-04-04 19:30:20 -0300

    TG-8 separando el contenido del footer

--- index.php
+++ index.php
@@ -1,154 +1,150 @@
 <?php
-    require_once 'header.php';
+require_once 'header.php';
 ?>
-    <div class="row">
-                <div class="col-md-6">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>
-                                    #
-                                </th>
-                                <th>
-                                    Barco
-                                </th>
-                                <th>
-                                    Coordenadas
-                                </th>
-                                <th>
-                                    Alineación
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr class="table-active">
-                                <td>
-                                    1
-                                </td>
-                                <td>
-                                    Barco 1
-                                </td>
-                                <td>
-                                    <input size="2" type="text" /> -
-                                    <input size="2" type="text" />
-                                </td>
-                                <td>
-                                    <select>
+<div class="row">
+    <div class="col-md-6">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>
+                        #
+                    </th>
+                    <th>
+                        Barco
+                    </th>
+                    <th>
+                        Coordenadas
+                    </th>
+                    <th>
+                        Alineación
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="table-active">
+                    <td>
+                        1
+                    </td>
+                    <td>
+                        Barco 1
+                    </td>
+                    <td>
+                        <input size="2" type="text" /> -
+                        <input size="2" type="text" />
+                    </td>
+                    <td>
+                        <select>
 
-                                        <option>
-                                            Horizontal
-                                        </option>
-                                    </select>
-                                </td>
-                            </tr>
-                            <tr class="table-active">
-                                <td>
-                                    2
-                                </td>
-                                <td>
-                                    Barco 2
-                                </td>
-                                <td>
-                                    <input size="2" type="text" /> -
-                                    <input size="2" type="text" />
-                                </td>
-                                <td>
-                                    <select>
+                            <option>
+                                Horizontal
+                            </option>
+                        </select>
+                    </td>
+                </tr>
+                <tr class="table-active">
+                    <td>
+                        2
+                    </td>
+                    <td>
+                        Barco 2
+                    </td>
+                    <td>
+                        <input size="2" type="text" /> -
+                        <input size="2" type="text" />
+                    </td>
+                    <td>
+                        <select>
 
-                                        <option>
-                                            Horizontal
-                                        </option>
-                                    </select>
-                                </td>
-                            </tr>
-                            <tr class="table-active">
-                                <td>
-                                    3
-                                </td>
-                                <td>
-                                    Barco 3
-                                </td>
-                                <td>
-                                    <input size="2" type="text" /> -
-                                    <input size="2" type="text" />
-                                </td>
-                                <td>
-                                    <select>
+                            <option>
+                                Horizontal
+                            </option>
+                        </select>
+                    </td>
+                </tr>
+                <tr class="table-active">
+                    <td>
+                        3
+                    </td>
+                    <td>
+                        Barco 3
+                    </td>
+                    <td>
+                        <input size="2" type="text" /> -
+                        <input size="2" type="text" />
+                    </td>
+                    <td>
+                        <select>
 
-                                        <option>
-                                            Vertical
-                                        </option>
-                                    </select>
-                                </td>
-                            </tr>
-                            <tr class="table-active">
-                                <td>
-                                    4
-                                </td>
-                                <td>
-                                    Barco 4
-                                </td>
-                                <td>
-                                    <input size="2" type="text" /> -
-                                    <input size="2" type="text" />
-                                </td>
-                                <td>
-                                    <select>
+                            <option>
+                                Vertical
+                            </option>
+                        </select>
+                    </td>
+                </tr>
+                <tr class="table-active">
+                    <td>
+                        4
+                    </td>
+                    <td>
+                        Barco 4
+                    </td>
+                    <td>
+                        <input size="2" type="text" /> -
+                        <input size="2" type="text" />
+                    </td>
+                    <td>
+                        <select>
 
-                                        <option>
-                                            Horizontal
-                                        </option>
-                                    </select>
-                                </td>
-                            </tr>
-                            <tr class="table-active">
-                                <td>
-                                    5
-                                </td>
-                                <td>
-                                    Barco 5
-                                </td>
-                                <td>
-                                    <input size="2" type="text" /> -
-                                    <input size="2" type="text" />
-                                </td>
-                                <td>
-                                    <select>
+                            <option>
+                                Horizontal
+                            </option>
+                        </select>
+                    </td>
+                </tr>
+                <tr class="table-active">
+                    <td>
+                        5
+                    </td>
+                    <td>
+                        Barco 5
+                    </td>
+                    <td>
+                        <input size="2" type="text" /> -
+                        <input size="2" type="text" />
+                    </td>
+                    <td>
+                        <select>
 
-                                        <option>
-                                            Horizontal
-                                        </option>
-                                    </select>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div class="col-md-6">
-                    <img alt="Bootstrap Image Preview" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Schiffeversenk.jpg/220px-Schiffeversenk.jpg" />
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-6">
-                    <form role="form">
-                        <div class="form-group">
+                            <option>
+                                Horizontal
+                            </option>
+                        </select>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="col-md-6">
+        <img alt="Bootstrap Image Preview" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Schiffeversenk.jpg/220px-Schiffeversenk.jpg" />
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-6">
+        <form role="form">
+            <div class="form-group">
 
-                            <label for="exampleInputEmail1">
-                                Usuario
-                            </label>
-                            <input class="form-control" id="exampleInputEmail1" type="email" />
-                        </div> 
-                        <button type="submit" class="btn btn-primary">
-                            Submit
-                        </button>
-                    </form>
-                </div>
-                <div class="col-md-6">
-                </div>
-            </div>
-        </div>
-        <script src="js/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/scripts.js"></script>
-    </body>
-</html>
+                <label for="exampleInputEmail1">
+                    Usuario
+                </label>
+                <input class="form-control" id="exampleInputEmail1" type="email" />
+            </div> 
+            <button type="submit" class="btn btn-primary">
+                Submit
+            </button>
+        </form>
+    </div>
+    <div class="col-md-6">
+    </div>
+</div>
+<?php
+require_once 'footer.php';
--- paginajuego.php
+++ paginajuego.php
@@ -1,28 +1,24 @@
 <?php
-    require_once 'header.php';
+require_once 'header.php';
 ?>
-    <div class="row">
-                <div class="col-md-6">
-                    <img class="dummy" alt="Bootstrap Image Preview" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Schiffeversenk.jpg/220px-Schiffeversenk.jpg" />
-                </div>
-                <div class="col-md-6">
-                    <img class="dummy" alt="Bootstrap Image Preview" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Schiffeversenk.jpg/220px-Schiffeversenk.jpg" />
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-5">
-                    <img alt="Jugador 1" src="images/user.png" />
-                </div>
-                <div class="col-md-2">
-                    Jugador 1 Vs Computadora
-                </div>
-                <div class="col-md-5">
-                    <img alt="Jugador 1" src="images/user.png" />
-                </div>
-            </div>
-        </div>
-        <script src="js/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/scripts.js"></script>
-    </body>
-</html>
+<div class="row">
+    <div class="col-md-6">
+        <img class="dummy" alt="Bootstrap Image Preview" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Schiffeversenk.jpg/220px-Schiffeversenk.jpg" />
+    </div>
+    <div class="col-md-6">
+        <img class="dummy" alt="Bootstrap Image Preview" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Schiffeversenk.jpg/220px-Schiffeversenk.jpg" />
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-5">
+        <img alt="Jugador 1" src="images/user.png" />
+    </div>
+    <div class="col-md-2">
+        Jugador 1 Vs Computadora
+    </div>
+    <div class="col-md-5">
+        <img alt="Jugador 1" src="images/user.png" />
+    </div>
+</div>
+<?php
+require_once 'footer.php';
